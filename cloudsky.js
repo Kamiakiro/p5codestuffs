c0dc784a3ffacc29fa565c9f44a43e5ae66f7ef3
Kamiakiro <pasaggabriel@gmail.com>
2026-09-18 11:34:19 -0400

    Add files via upload

---
+++ cloudsky.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(500, 500);
+  background("skyblue");
+  fill (130,205,235);
+}
+
+
+function draw() {
+
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(350, 90, 200, 100);
+ 
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(390, 110, 210, 90);
+ 
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(5, 50, 300, 55);
+ 
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(490, 290, 300, 95);
+
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(350, 300, 310, 100);
+
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(220, 360, 350, 80);
+
+ fill(255, 255, 255);
+ strokeWeight(0);
+ ellipse(30, 100, 320, 90);
+ 
+ fill(120, 170, 200);
+ strokeWeight(0);
+ rect(0, 370, 500, 150);
+
+ fill(105, 155, 190);
+ strokeWeight(0);
+ rect(0, 440, 500, 150);
+
+ fill(255, 142, 102);
+ strokeWeight(10);
+ ellipse(90, 400, 450, 450);
+ 
+}
+
+
+
+//a bmcc student was here >:)
+
